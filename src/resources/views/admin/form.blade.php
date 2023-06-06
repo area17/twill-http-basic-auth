acc155cb55eff4575f0ffbd0005f1b9a3a82fc44
Joyce Verheije <joyce@area17.com>
2023-06-06 02:07:19 +0200

    Fix facade injection in Twill 3 form

--- src/resources/views/admin/form.blade.php
+++ src/resources/views/admin/form.blade.php
@@ -4,37 +4,36 @@
     use A17\TwillHttpBasicAuth\Support\Facades\TwillHttpBasicAuth;
 @endphp
 
+@inject('twillHttpBasicAuth', TwillHttpBasicAuth::class)
+
 @section('contentFields')
+
     @formField('input', [
-    'type' => TwillHttpBasicAuth::config('inputs.username.type'),
-    'name' => 'username',
-    'label' => 'Username',
-    'required' => true,
-    'disabled' => TwillHttpBasicAuth::hasDotEnv(),
+        'type' => $twillHttpBasicAuth::config('inputs.username.type'),
+        'name' => 'username',
+        'label' => 'Username',
+        'required' => true,
+        'disabled' => $twillHttpBasicAuth::hasDotEnv(),
     ])
 
     @formField('input', [
-    'type' => TwillHttpBasicAuth::config('inputs.password.type'),
-    'name' => 'password',
-    'label' => 'Password',
-    'required' => true,
-    'disabled' => TwillHttpBasicAuth::hasDotEnv(),
-    'password' => true,
+        'type' => $twillHttpBasicAuth::config('inputs.password.type'),
+        'name' => 'password',
+        'label' => 'Password',
+        'required' => true,
+        'disabled' => $twillHttpBasicAuth::hasDotEnv(),
+        'password' => true,
     ])
 
     @formField('checkbox', [
-    'name' => 'allow_laravel_login',
-
-    'label' => 'Allow Laravel users to login',
-
-    'disabled' => TwillHttpBasicAuth::hasDotEnv(),
+        'name' => 'allow_laravel_login',
+        'label' => 'Allow Laravel users to login',
+        'disabled' => $twillHttpBasicAuth::hasDotEnv(),
     ])
 
     @formField('checkbox', [
-    'name' => 'allow_twill_login',
-
-    'label' => "Allow Twill users to login",
-
-    'disabled' => TwillHttpBasicAuth::hasDotEnv(),
+        'name' => 'allow_twill_login',
+        'label' => "Allow Twill users to login",
+        'disabled' => $twillHttpBasicAuth::hasDotEnv(),
     ])
 @stop
