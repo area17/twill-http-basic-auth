4a6ebb13630a8f82240c6d99506228b72f16213f
Antonio Ribeiro <antonio@area17.com>
2024-01-31 15:17:11 +0100

    Must be singular o Twill 2

--- src/Http/Controllers/TwillHttpBasicAuthController.php
+++ src/Http/Controllers/TwillHttpBasicAuthController.php
@@ -16,7 +16,7 @@ use A17\TwillHttpBasicAuth\Support\Facades\TwillHttpBasicAuth as TwillHttpBasicA
 
 class TwillHttpBasicAuthController extends ModuleController
 {
-    protected $moduleName = 'twillHttpBasicAuths';
+    protected $moduleName = 'twillHttpBasicAuth';
 
     protected $titleColumnKey = 'domain_string';
 
